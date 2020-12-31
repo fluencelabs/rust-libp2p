0d50ef081e63d63d8daba2494be2c7b16f65fe68
folex <0xdxdy@gmail.com>
2020-12-31 15:00:29 +0300

    fix trust-graph dependency

--- misc/core-derive/tests/test.rs
+++ misc/core-derive/tests/test.rs
@@ -18,7 +18,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-use libp2p_core_derive::*;
+use fluence_fork_libp2p_core_derive::*;
 
 /// Small utility to check that a type implements `NetworkBehaviour`.
 #[allow(dead_code)]
